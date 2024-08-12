18241b46178867bd976850f3107f54a80ce1d2fe
Theo <tursunboyev573@gmail.com>
2024-08-12 23:58:53 +0900

    feat:add Authors

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 console.log("Web Serverni boshlash");
-
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data)=> {
+  if(err) {
+    console.log("ERROR:",err);
+  } else {
+    user = JSON.parse(data)
+  }
+});
 
 // 1 Kirish kodlar
 app.use(express.static("public")); //imiglar uchun
@@ -23,9 +33,13 @@ app.set("view engine", "ejs");
 });*/
 
 app.post("/create-item", (req,res) => {
-      console.log(req.body);
-      res.json({test:"success"});
+     // console.log(req.body);
+      //res.json({test:"success"});
 });
+
+app.get('/author', (req, res) => {
+  res.render("author", {user: user});
+})
 
 app.get("/",function (req,res) {
       res.render('harid');
