be03adab6bfd94aff06526068080b8ae0309a565
Theo <tursunboyev573@gmail.com>
2024-08-21 23:43:18 +0900

    feat: mongoDB connection

---
+++ app.js
@@ -0,0 +1,53 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data)=> {
+  if(err) {
+    console.log("ERROR:",err);
+  } else {
+    user = JSON.parse(data)
+  }
+});
+
+//MongoDB connect
+const db = require("./server").db();
+
+// 1 Kirish kodlar
+app.use(express.static("public")); //imiglar uchun
+app.use(express.json()); // datani object ga ugartiradi 
+app.use(express.urlencoded({ extended: true })); //html dan form request qiladi
+
+// 2 Session ga bogliq
+
+// 3   Views ga bogliq codlar
+//ejs orqali frontentni yasaymiz backendni ichida
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing ga bogliq code
+
+/*app.get("/Class", function (req, res) {
+  res.end(`<h1 style="background: green" >Hello Theo welcome to MIT </h1>`);
+});*/
+
+app.post("/create-item", (req,res) => {
+     // console.log(req.body);
+      //res.json({test:"success"});
+});
+
+// Request => Tradational & Rest (API) & Grahql(Nester)
+// Request => Header & body
+
+app.get('/author', (req, res) => {
+  res.render("author", {user: user});
+})
+
+app.get("/",function (req,res) {
+      res.render('reja');
+})
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,55 +1,26 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+//const app = require("./app");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data)=> {
-  if(err) {
-    console.log("ERROR:",err);
-  } else {
-    user = JSON.parse(data)
-  }
+let db;
+const connectionString = "mongodb+srv://javohir7707:theo200520@theo.v8cnr.mongodb.net"
+
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else {
+        console.log("MongoDB connection succeed")
+        module.exports = client;
+        const app = require("./app");
+       
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function () {
+    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+ 
+});
+    }
 });
 
-// 1 Kirish kodlar
-app.use(express.static("public")); //imiglar uchun
-app.use(express.json()); // datani object ga ugartiradi 
-app.use(express.urlencoded({ extended: true })); //html dan form request qiladi
-
-// 2 Session ga bogliq
-
-// 3   Views ga bogliq codlar
-//ejs orqali frontentni yasaymiz backendni ichida
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing ga bogliq code
-
-/*app.get("/Class", function (req, res) {
-  res.end(`<h1 style="background: green" >Hello Theo welcome to MIT </h1>`);
-});*/
-
-app.post("/create-item", (req,res) => {
-     // console.log(req.body);
-      //res.json({test:"success"});
-});
-
-// Request => Tradational & Rest (API) & Grahql(Nester)
-// Request => Header & body
-
-app.get('/author', (req, res) => {
-  res.render("author", {user: user});
-})
-
-app.get("/",function (req,res) {
-      res.render('reja');
-})
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-
-});
